e797ea3d3bf4c5d9e4d8036b9ea62566b7da46a5
Ethan <camposej05@gmail.com>
2024-03-26 11:14:20 -0500

    Added two methods that utilize lists and HashMaps respectively.

--- ClassNotes/src/main/java/org/example/Main.java
+++ ClassNotes/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static input.InputUtils.intInput;
 import static input.InputUtils.stringInput;
+import static input.InputUtils.yesNoInput;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,13 +27,23 @@ public class Main {
         int counter = intInput("How many times do you want that to repeat? ");
         String repeated = repeatThePhrase(phrasing, counter);
         System.out.println(repeated);
+
+        List<String> fruitList = fruitCount(); //New
+        if (fruitList.isEmpty()){
+            System.out.println("The list is empty.");
+        } else {
+            System.out.print(fruitList);
+        }
+
+        HashMap<String, String> favoriteMediaList = favoriteMedia(); //New
+        if (favoriteMediaList.isEmpty()){
+            System.out.println("The HashMap is empty.");
+        } else {
+            System.out.println(favoriteMediaList);
+        }
     }
     public static boolean expenses(int amountOfBananas) {
-        if (amountOfBananas > 3){
-            return true;
-        } else {
-            return false;
-        }
+        return amountOfBananas > 3;
     }
 
     public static String repeatThePhrase(String phrasing, int counter){
@@ -36,5 +52,44 @@ public class Main {
             concatment.append(phrasing);
         }
         return concatment.toString();
+    }
+
+    public static List<String> fruitCount() { //New
+
+        List<String> fruitList = new ArrayList<>();
+        System.out.println("Let's build a list of fruits to buy!");
+
+        while (yesNoInput("Add more? ")) {
+            String whatFruit = stringInput("Enter a fruit: ");
+            fruitList.add(whatFruit);
+        }
+
+        if(fruitList.isEmpty()){
+            boolean confirm = yesNoInput("The list is empty.  Are you sure you want to continue?");
+            if(!confirm){
+                fruitCount();
+            }
+        }
+        return fruitList;
+    }
+
+    public static HashMap<String, String> favoriteMedia() { //New
+
+        HashMap<String, String> mediaList = new HashMap<>();
+        System.out.println("Let's build a HashMap of your favorite media!\nPlease enter the name of the media and what form it is in when prompted!");
+
+        while(yesNoInput("Add more?")) {
+            String mediaName = stringInput("Name: ");
+            String mediaForm = stringInput("Form: ");
+            mediaList.put(mediaName, mediaForm);
+        }
+
+        if(mediaList.isEmpty()){
+            boolean confirm = yesNoInput("The HashMap is empty.  Are you sure you want to continue?");
+            if (!confirm){
+                favoriteMedia();
+            }
+        }
+        return mediaList;
     }
 }
